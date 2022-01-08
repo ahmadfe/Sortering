ad32721ad96663ad62fb1f190ce6020d00e0b9f5
ahmadfe <ahmadfe@live.com>
2022-01-08 21:34:23 +0100

    first commit

---
+++ src/Sort/SortMain.java
@@ -0,0 +1,23 @@
+package src.Sort;
+
+public class SortMain {
+    public static void main(String[] args) {
+
+      int[] unsorted;
+      if(args.length > 0) {
+          unsorted = new int[args.length];
+          try {
+              for(int i = 0; i < args.length; i++)
+                  unsorted[i] = Integer.valueOf(args[i]);
+
+              Sorter.sort(unsorted);
+          } catch(Exception e) {
+              System.out.println("Wrong Input");
+          }
+
+      } else {
+          SortTest.test();
+      }
+      //int[] unsorted = new int[]{76,11,-55,2,16,-21,7,88};
+    }
+}        
---
+++ src/Sort/SortTest.java
@@ -0,0 +1,21 @@
+package src.Sort;
+
+public class SortTest {
+
+    public static void test() {
+
+        
+        int[] test1 = {-1, -95, 49, 0, 18, 9};
+        int[] test2 = {3, 80, -15, 27, -100};
+        int[] test3 = {104, 0, -294, -153, -24};
+        int[] test4 = {201, -491, 1000, 0, 500};
+        int[] test5 = {5, 35, -34, 33, -31, 0};
+
+        Sorter.sort(test1);
+        Sorter.sort(test2);
+        Sorter.sort(test3);
+        Sorter.sort(test4);
+        Sorter.sort(test5);
+
+    }
+}
